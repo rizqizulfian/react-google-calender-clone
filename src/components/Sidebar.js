7ff4ae86d4fcbc3d8c035ce4d8670851cac933bf
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-02-27 01:52:35 +0700

    [010] - Add SmallCalendar header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import CreateEventButton from './CreateEventButton';
+import SmallCalendar from './SmallCalendar';
 
 const Sidebar = () => {
   return (
     <aside className="border p-5 w-64">
       <CreateEventButton />
+      <SmallCalendar />
     </aside>
   )
 }
--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+
+import { getMonth } from '../utils';
 
 const SmallCalendar = () => {
+  const [currentMonthIdx, setCurrentMonthIdx] = useState(dayjs().month());
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(currentMonthIdx));
+  }, [currentMonthIdx]);
+
   return (
-    <div>
+    <div className="mt-9">
+      <header className="flex justify-between">
+        <p className="text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
+        </p>
+      </header>
 
     </div>
   )
