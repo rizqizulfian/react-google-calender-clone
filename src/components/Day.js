e6b50f5aad31b648a77c72053dd9f08f151687c1
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-02-27 00:12:42 +0700

    [005] - Formatting UI Day

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Day = ({day}) => {
+const Day = ({ day }) => {
   return (
-    <div>
-      {day.format()}
+    <div className="border border-gray-200 flex flex-col">
+      <header className="flex flex-col items-center">
+        <p className="text-sm mt-1">{day.format('ddd').toUpperCase()}</p>
+        <p className="text-sm p-1 my-1 text-center">
+          {day.format('DD')}
+        </p>
+      </header>
     </div>
   );
 };
