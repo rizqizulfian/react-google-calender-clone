e41ad8ac964020cadacbff4db48f58a136f4f8e5
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-02-27 02:10:27 +0700

    [013] - Add getDayClass

--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -26,22 +26,36 @@ const SmallCalendar = () => {
     setCurrentMonthIdx(currentMonthIdx + 1);
   };
 
+  const getDayClass = (day) => {
+    const format = "DD-MM-YY";
+    const nowDay = dayjs().format(format);
+    const currDay = day.format(format);
+
+    if (nowDay === currDay) {
+      return 'bg-blue-500 rounded-full text-white';
+    } else {
+      return '';
+    }
+  };
+
   return (
     <div className="mt-9">
       <header className="flex justify-between">
         <p className="text-gray-500 font-bold">
           {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
         </p>
-        <button onClick={handlePrevMonth}>
-          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-            chevron_left
-          </span>
-        </button>
-        <button onClick={handleNextMonth}>
-          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-            chevron_right
-          </span>
-        </button>
+        <div>
+          <button onClick={handlePrevMonth}>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+              chevron_left
+            </span>
+          </button>
+          <button onClick={handleNextMonth}>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+              chevron_right
+            </span>
+          </button>
+        </div>
       </header>
       <div className="grid grid-cols-7 grid-rows-6">
         {currentMonth[0].map((day, i) => (
@@ -52,7 +66,7 @@ const SmallCalendar = () => {
         {currentMonth.map((row, i) => (
           <React.Fragment key={i}>
             {row.map((day, idx) => (
-              <button key={idx} className={`py-1 w-full`}>
+              <button key={idx} className={`py-1 w-full ${getDayClass(day)}`}>
                 <span className="text-sm">
                   {day.format('D')}
                 </span>
